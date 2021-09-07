434a7dbe0b0a5c64e720568afcbcf801bd7aa8c6
nkleinmann <nekleinmann@gmail.com>
2021-09-07 11:47:49 -0400

    Update solution

--- rest-rant.sql
+++ rest-rant.sql
@@ -5,10 +5,10 @@
 CREATE TABLE public.place
 (
     place_id serial NOT NULL,
-    place_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    pic character varying COLLATE pg_catalog."default",
-    cuisines character varying COLLATE pg_catalog."default" NOT NULL,
-    state character varying COLLATE pg_catalog."default",
+    place_name character varying(255) NOT NULL,
+    pic character varying,
+    cuisines character varying NOT NULL,
+    state character varying,
     founded smallint,
     CONSTRAINT place_pkey PRIMARY KEY (place_id)
 )
@@ -22,10 +22,10 @@ CREATE TABLE public.comment
 (
     comment_id serial NOT NULL,
     place_id smallint NOT NULL,
-    content text COLLATE pg_catalog."default",
+    content text,
     stars smallint NOT NULL,
     rant boolean,
-    author character varying COLLATE pg_catalog."default",
+    author character varying,
     CONSTRAINT comment_pkey PRIMARY KEY (comment_id),
     CONSTRAINT comment_place_id_fkey FOREIGN KEY (place_id)
         REFERENCES public.place (place_id) MATCH SIMPLE
