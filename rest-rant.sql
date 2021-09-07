39d547c3c6eb90c19e766056bff0f6f40f0dbcb7
nkleinmann <nekleinmann@gmail.com>
2021-09-07 10:42:58 -0400

    Add part of bonus

--- rest-rant.sql
+++ rest-rant.sql
@@ -1,39 +1,70 @@
--- Table: public.place
+-- Database: new_rest_rant
 
--- DROP TABLE public.place;
+-- DROP DATABASE new_rest_rant;
 
-CREATE TABLE public.place
+CREATE DATABASE new_rest_rant
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+-- Table: public.places
+
+-- DROP TABLE public.places;
+
+CREATE TABLE public.places
 (
-    place_id serial NOT NULL,
-    place_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    pic character varying COLLATE pg_catalog."default",
-    cuisines character varying COLLATE pg_catalog."default" NOT NULL,
-    state character varying COLLATE pg_catalog."default",
-    founded smallint,
+    place_id integer NOT NULL,
+    name character varying NOT NULL,
+    state character varying,
+    cuisines character varying,
+    pic character varying,
+    founded integer,
     CONSTRAINT place_pkey PRIMARY KEY (place_id)
 )
 
-TABLESPACE pg_default;
+-- ALTER TABLE public.places
+--     OWNER to postgres;
 
-ALTER TABLE public.place
-    OWNER to postgres;
-	
-CREATE TABLE public.comment
+-- Table: public.comments
+
+-- DROP TABLE public.comments;
+
+CREATE TABLE public.comments
 (
-    comment_id serial NOT NULL,
+    comment_id integer NOT NULL,
     place_id smallint NOT NULL,
-    content text COLLATE pg_catalog."default",
+    content character varying,
     stars smallint NOT NULL,
     rant boolean,
-    author character varying COLLATE pg_catalog."default",
-    CONSTRAINT comment_pkey PRIMARY KEY (comment_id),
-    CONSTRAINT comment_place_id_fkey FOREIGN KEY (place_id)
-        REFERENCES public.place (place_id) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT
+    author character varying,
+    CONSTRAINT comments_pkey PRIMARY KEY (comment_id)
 )
 
-TABLESPACE pg_default;
+-- ALTER TABLE public.comments
+--     OWNER to postgres;
 
-ALTER TABLE public.comment
-    OWNER to postgres;
+-- Table: public.places_comments
+
+-- DROP TABLE public.places_comments;
+
+CREATE TABLE public.places_comments
+(
+    comment_id integer,
+    place_id integer,
+    CONSTRAINT comment_id FOREIGN KEY (comment_id)
+        REFERENCES public.comments (comment_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT place_id FOREIGN KEY (place_id)
+        REFERENCES public.places (place_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+-- ALTER TABLE public.places_comments
+--     OWNER to postgres;
